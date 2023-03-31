8fb244b2917571451eaa7316ba33d0f82b8ac501
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-03-31 10:35:49 -0300

    Facebook Login Provider

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -3,17 +3,15 @@ import { Link, Navigate } from "react-router-dom";
 import logoIcon from "../../assets/icons/livros.png";
 import googleIcon from "../../assets/icons/google-white.svg";
 import githubIcon from "../../assets/icons/github.svg"
+import facebookIcon from "../../assets/icons/facebook.svg"
 import { useForm } from "react-hook-form";
-import { cadastrarEmailSenha, loginGithub, loginGoogle } from "../../firebase/auth";
+import { cadastrarEmailSenha, loginFacebook, loginGithub, loginGoogle } from "../../firebase/auth";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 
 export function Cadastro() {
-
-
-
 
   const {
     register,
@@ -76,8 +74,25 @@ export function Cadastro() {
         });
       });
   }
-  const usuarioLogado = useContext(AuthContext);
 
+  function onLoginFacebook() {
+    loginFacebook().then((user) => {
+      toast.success(`Bem-vindo(a) ${user.displayName}`, {
+        position: "bottom-right",
+        duration: 2500,
+      });
+      navigate("/");
+    })
+      .catch((erro) => {
+        // tratamento de erro
+        toast.error(`Um erro aconteceu. Código: ${erro.code}`, {
+          position: "bottom-right",
+          duration: 2500,
+        });
+      });
+  }
+
+  const usuarioLogado = useContext(AuthContext);
   if (usuarioLogado === null) {
     // se está deslogado
     // redireciona para a página de login
@@ -85,52 +100,57 @@ export function Cadastro() {
   }
 
   return (
-    <Container fluid className="my-5">
-      <p className="text-center">
-        <img src={logoIcon} width="256" alt="Logo do app" />
-      </p>
-      <h4>Faça parte da nossa plataforma</h4>
-      <p className="text-muted">
-        Já tem conta? <Link to="/login">Entre</Link>
-      </p>
-      <hr />
-      <Button className="mb-1" variant="danger" onClick={onLoginGoogle}>
-        <img src={googleIcon} width="32" alt="Logo do google" /> Entrar com o Google
-      </Button>
-      <br />
-      <Button className="mb-3" variant="dark" onClick={onLoginGithub}>
-        <img src={githubIcon} width="32" alt="Logo do github" /> Entrar com o Github
-      </Button>
+    <Container fluid className="container-login-cadastro">
+      <div className="login-cadastro my-3">
+        <p className="text-center">
+          <img src={logoIcon} width="256" alt="Logo do app" />
+        </p>
+        <h4>Faça parte da nossa plataforma</h4>
+        <p className="text-muted">
+          Já tem conta? <Link to="/login">Entre</Link>
+        </p>
+        <hr />
 
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            className={errors.email && "is-invalid"}
-            placeholder="Seu email"
-            {...register("email", { required: "O email é obrigatório" })}
-          />
-          <Form.Text className="invalid-feedback">
-            {errors.email?.message}
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Senha</Form.Label>
-          <Form.Control
-            type="password"
-            className={errors.senha && "is-invalid"}
-            placeholder="Sua senha"
-            {...register("senha", { required: "A senha é obrigatória" })}
-          />
-          <Form.Text className="invalid-feedback">
-            {errors.senha?.message}
-          </Form.Text>
-        </Form.Group>
-        <Button type="submit" variant="success">
-          Cadastrar
-        </Button>
-      </Form>
+        <div className="d-flex flex-column" >
+          <Button className="mb-1" variant="danger" onClick={onLoginGoogle}>
+            <img src={googleIcon} width="32" alt="Logo do google" /> Entrar com o Google
+          </Button>
+          <Button className="mb-1" variant="dark" onClick={onLoginGithub}>
+            <img src={githubIcon} width="32" alt="Logo do github" /> Entrar com o Github
+          </Button>
+          <Button className="mb-3" variant="primary" onClick={onLoginFacebook}>
+            <img src={facebookIcon} width="32" alt="Logo do github" /> Entrar com o Facebook
+          </Button>
+        </div>
+
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              className={errors.email && "is-invalid"}
+              placeholder="Seu email"
+              {...register("email", { required: "O email é obrigatório" })}
+            />
+            <Form.Text className="invalid-feedback">
+              {errors.email?.message}
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Senha</Form.Label>
+            <Form.Control
+              type="password"
+              className={errors.senha && "is-invalid"}
+              placeholder="Sua senha"
+              {...register("senha", { required: "A senha é obrigatória" })}
+            />
+            <Form.Text className="invalid-feedback">
+              {errors.senha?.message}
+            </Form.Text>
+          </Form.Group>
+          <Button type="submit" variant="success">Cadastrar</Button>
+        </Form>
+      </div>
     </Container>
   );
 }
